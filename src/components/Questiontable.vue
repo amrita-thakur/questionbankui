01a9c7b81515cbd3b38818e2a867cf5366dfbe0c
Amrita <70787507+amrita20@users.noreply.github.com>
2020-12-22 20:47:43 +0545

    Update Questiontable.vue

--- src/components/Questiontable.vue
+++ src/components/Questiontable.vue
@@ -41,10 +41,10 @@ export default {
   },
   created() {
     this.$axios
-      .get("https://questionbankioedbms.herokuapp.com/getpapers")
+      .get("https://questionbankioe.herokuapp.com/getpapers")
       .then(response => (this.papers = response.data.paper_list));
     this.$axios
-      .get("https://questionbankioedbms.herokuapp.com/getcourses")
+      .get("https://questionbankioe.herokuapp.com/getcourses")
       .then(response => (this.courses = response.data.course_list));
   },
   methods: {}
